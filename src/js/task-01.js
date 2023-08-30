4819ecdf34830929b165f103bd7a754a101dec80
Max-Arslanov <airexpresskiev@gmail.com>
2023-08-30 22:14:23 +0300

    Categories

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,10 @@
-const categoriesEl = document.querySelector('#categories');
-console.log(categoriesEl);
+// const categoriesEl = document.querySelector('#categories');
+// console.log(categoriesEl);
+const categoriesDetEl = document.querySelectorAll('.item');
+// console.log(categoriesDetEl);
+let count = 0;
+categoriesDetEl.forEach(() => {
+  count++;
+});
+
+console.log('Number of categories:', count);
