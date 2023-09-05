adb202cc08d215af28c478254314d5775db43bdc
Max-Arslanov <airexpresskiev@gmail.com>
2023-09-05 22:52:58 +0300

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = this.elements.email.value;
+  const password = this.elements.password.value;
+
+  // Перевірка введених даних
+  if (email === "example@example.com" && password === "password") {
+    alert("Success");
+    // Тут ви можете виконати подальші дії, наприклад, перенаправити користувача на іншу сторінку
+  } else {
+    alert("Error");
+  }
+});
