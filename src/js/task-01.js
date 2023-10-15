0d2a8b1b905c305f8a6e0e5588a83ae9497e0563
Max-Arslanov <133591144+Max-Arslanov@users.noreply.github.com>
2023-10-15 19:15:32 +0300

    task1 update

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,23 +1,16 @@
-
-const categoriesDetEl = document.querySelectorAll('.item');
-
+const categories = document.querySelectorAll('.item');
 let count = 0;
-categoriesDetEl.forEach(() => {
-  count++;
+categories.forEach(() => {count++;
 });
+console.log("Number of categories:", count);
 
-console.log('Number of categories:', count);
-categoriesDetEl.forEach((categoryItem) => {
+categories.forEach((categoryItem) => {
   
   const titleElement = categoryItem.querySelector('h2');
   const subItems = categoryItem.querySelectorAll('li');
   console.log(`Category: ${titleElement.textContent}`);
   console.log(`Elements: ${subItems.length}`);
 });
-
-
-
-
 
 
 
