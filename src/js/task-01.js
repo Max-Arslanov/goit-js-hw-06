d197c0bcc130017d02d0ff0538323326a5623e3c
Max-Arslanov <133591144+Max-Arslanov@users.noreply.github.com>
2023-10-15 18:43:35 +0300

    JS task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,6 @@
-// const categoriesEl = document.querySelector('#categories');
-// console.log(categoriesEl);
+
 const categoriesDetEl = document.querySelectorAll('.item');
-// console.log(categoriesDetEl);
+
 let count = 0;
 categoriesDetEl.forEach(() => {
   count++;
